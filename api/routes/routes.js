a8ce8eeb685eec03cd0a9c8537f7c7b358a81ad9
Joel Álvarez Myrrie <devmyrrie@gmail.com>
2017-05-29 09:04:52 -0600

    fix: Routes are in same file

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
+const folderApi = require('../controllers/foldersController');
 const notesController = require('../controllers/notesController');
+
+
+router.get('/folders', folderApi.getFolders);
+router.post('/folders', folderApi.addFolder);
+router.get('/folders/:id', folderApi.getFolderById);
+router.delete('/folders/:id', folderApi.deleteFolderById);
+router.put('/folders/:id', folderApi.modifyFolderById);
+
 
 router.get('/notes', notesController.index);
 router.post('/notes', notesController.create);
